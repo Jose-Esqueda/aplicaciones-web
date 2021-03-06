2859c35801f4f318e9148df4b2575cc02c1bb579
Jose-Esqueda <150206@upbc.edu.mx>
2021-03-05 20:44:42 -0800

    Ejercicio 6: Listado de series y peliculas

---
+++ aspnet/TespApp/TespApp/Controllers/TVShowsMoviesController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TespApp.Models;
+
+namespace TespApp.Controllers
+{
+    public class TVShowsMoviesController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<TVShowsMoviesIndexViewModel> TvShowsList = new List<TVShowsMoviesIndexViewModel>();
+            var show = new TVShowsMoviesIndexViewModel()
+            {
+                AnnoSalida = 1974,
+                genero = "Terror",
+                Nombre = "Masacre en texas",
+                Tipo = "R"
+            };
+            TvShowsList.Add(show);
+            show = new TVShowsMoviesIndexViewModel()
+            {
+                AnnoSalida = 2000,
+                genero = "Terror",
+                Nombre = "El Aro",
+                Tipo = "R"
+            };
+            TvShowsList.Add(show);
+            show = new TVShowsMoviesIndexViewModel()
+            {
+                AnnoSalida = 2005,
+                genero = "Aventura",
+                Nombre = "El señor de los anillos",
+                Tipo = "8+"
+            };
+            TvShowsList.Add(show);
+            show = new TVShowsMoviesIndexViewModel()
+            {
+                AnnoSalida = 2005,
+                genero = "Ciencia ficcion",
+                Nombre = "Viaje al espacio",
+                Tipo = "8+"
+            };
+            TvShowsList.Add(show);
+
+            return View(TvShowsList);
+        }
+    }
+}
---
+++ aspnet/TespApp/TespApp/Models/TVShowsMoviesIndexViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TespApp.Models
+{
+    public class TVShowsMoviesIndexViewModel
+    {
+        public string Tipo { get; set; }
+        public string Nombre { get; set; }
+        public int AnnoSalida { get; set; }
+        public string genero { get; set; }
+    }
+}
